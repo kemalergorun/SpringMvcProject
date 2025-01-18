8cb5ae0e084df451919edcff1a9a8053b2edb78f
kemalergorun <kemalergorun@gmail.com>
2025-01-18 23:13:35 +0300

    180125

--- src/main/java/com/tpe/WebAppInitializer.java
+++ src/main/java/com/tpe/WebAppInitializer.java
@@ -2,23 +2,24 @@ package com.tpe;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-//web xml file replacement java based web applications
+//web xml file replacement java based web applications web xml yerine kullan
+//dispatcher servlet başlatılması için configure için
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
-    protected Class<?>[] getRootConfigClasses() {
+    protected Class<?>[] getRootConfigClasses() {//hibernate ve jdbc class config for
         return new Class[]{
                 RootConfig.class
         };
     }
 
     @Override
-    protected Class<?>[] getServletConfigClasses() {
+    protected Class<?>[] getServletConfigClasses() {//view resolver,handler mapping,
         return new Class[]{
                 WebMvcConfig.class
         };
     }
 
-    @Override //All the requests with incoming URL.
+    @Override //All the requests with incoming URL servlet tarafından karşılanır.
     protected String[] getServletMappings() {
         return new String[]{
                 "/" // -> /students/new/1 -> Still starts with a "/", so, it will be included.
--- src/main/java/com/tpe/WebMvcConfig.java
+++ src/main/java/com/tpe/WebMvcConfig.java
@@ -11,14 +11,14 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @ComponentScan("com.tpe")
-@EnableWebMvc
+@EnableWebMvc//mvc etkinleşir
 public class WebMvcConfig implements WebMvcConfigurer {
-
+    //view resolver objesi oluşturuldu
     @Bean
     public InternalResourceViewResolver resolver(){
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
+        resolver.setPrefix("/WEB-INF/views/");//başlangıçlar öneki
+        resolver.setSuffix(".jsp");//uzantılar suffix
         //Let's say that controller sent "students" as view name.
         //If controller sends the view name students: prefix + students + suffix = /WEB-INF/views/students.jsp
         resolver.setViewClass(JstlView.class);
@@ -26,9 +26,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         return resolver;
     }
 
+    //statik istekler servlete gerek yok webserver statik cvp verir
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
+        registry.addResourceHandler("/statics/**")
                 .addResourceLocations("/resources/") //Location of our static resources.
                 .setCachePeriod(0);
     }
